cde3cf29897b9ee5c945cfac2008eafbbfae6556
Alexander Pushkov <hi@ale.rocks>
2016-11-18 14:08:15 +0300

    Attribute LaTeX docs to Songbee Team, too

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ latex_elements = {}
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'Beerializer.tex', 'Beerializer Documentation',
-     'Alexander Pushkov', 'manual'),
+     author, 'manual'),
 ]
 
 #: The name of an image file (relative to this directory) to place at the top of
