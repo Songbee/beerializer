1b04b82c5ab93f1e32e9f4794997f4ba79920fa0
Alexander Pushkov <hi@ale.rocks>
2017-05-04 16:42:20 +0300

    Add JSONSchemaValidator

--- beerializer/validators.py
+++ beerializer/validators.py
@@ -1,5 +1,10 @@
 from .base import ValidationError
 
+try:
+    import jsonschema
+except ImportError:
+    jsonschema = None
+
 
 class EnumValidator(object):
 
@@ -10,3 +15,18 @@ class EnumValidator(object):
         if data not in self.choices:
             raise ValidationError("{} must be one of {}.  Got {}.".format(
                 field.name, self.choices, data))
+
+
+class JSONSchemaValidator:
+    def __init__(self, schema):
+        if jsonschema is None:
+            raise EnvironmentError("jsonschema must be installed for "
+                                   "JSONSchemaValidator to work. "
+                                   "(pip install jsonschema)")
+        self.schema = schema
+
+    def validate(self, field, data):
+        try:
+            jsonschema.validate(data, self.schema)
+        except jsonschema.ValidationError as je:
+            raise ValidationError(je.message) from je
